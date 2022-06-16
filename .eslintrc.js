51fcbc79650559b45db7394472134c2353c8bf21
mutec <mysterycode@mysterycode.de>
2022-06-16 23:24:06 +0200

    bump

---
+++ .eslintrc.js
@@ -0,0 +1,41 @@
+module.exports = {
+	root: true,
+	parser: "@typescript-eslint/parser",
+	parserOptions: {
+		tsconfigRootDir: __dirname,
+		project: ["./tsconfig.json"]
+	},
+	plugins: ["@typescript-eslint"],
+	extends: [
+		"eslint:recommended",
+		"plugin:@typescript-eslint/recommended",
+		"plugin:@typescript-eslint/recommended-requiring-type-checking",
+		"prettier"
+	],
+	rules: {
+		"@typescript-eslint/ban-types": [
+			"error", {
+				types: {
+					"object": false
+				},
+				extendDefaults: true
+			}
+		],
+		"@typescript-eslint/no-explicit-any": 0,
+		"@typescript-eslint/no-non-null-assertion": 0,
+		"@typescript-eslint/no-unsafe-assignment": 0,
+		"@typescript-eslint/no-unsafe-call": 0,
+		"@typescript-eslint/no-unsafe-member-access": 0,
+		"@typescript-eslint/no-unsafe-return": 0,
+		"@typescript-eslint/no-unused-vars": [
+			"error", {
+				"argsIgnorePattern": "^_"
+			}
+		],
+		"@typescript-eslint/no-misused-promises": [
+			"error", {
+				"checksVoidReturn": false
+			}
+		]
+	}
+};
